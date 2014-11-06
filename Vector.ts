13c77d60085d8a845ad6fae6eec6b5d464647b07
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-11-06 12:04:18 +0100

    finished base of vector class

--- Vector.ts
+++ Vector.ts
@@ -1,3 +1,22 @@
-/**
- * Created by Ark on 06-11-2014.
- */
+class Vector {
+    public constructor (
+        private x : number,
+        private y : number,
+        private z : number){}
+    public X() { return this.x; }
+    public Y() { return this.y; }
+    public Z() { return this.z; }
+    public add(v : Vector) : Vector {
+        return new Vector(this.x + v.x, this.y + v.y, this.z + v.z);
+    }
+    public sub(v : Vector) : Vector {
+        return new Vector(this.x - v.x, this.y - v.y, this.z - v.z);
+    }
+    public static Direction(pitch : number, yaw : number) : Vector {
+        return new Vector(
+            -Math.sin(yaw) * Math.cos(pitch),
+            Math.sin(pitch),
+            Math.cos(pitch) * Math.cos(yaw)
+        )
+    }
+}
