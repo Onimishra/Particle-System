6b07d26eb005cf0f10222dab6603f9845bc9c664
Nikolaj Korsbæk Falsted <nikolajfalsted@gmail.com>
2014-11-06 14:58:22 +0100

    no message

--- Emitter.ts
+++ Emitter.ts
@@ -13,6 +13,7 @@ class Emitter {
         private rateVar : number
     ) {
         this.particles = new Particle[this.limit];
+        this.nextEmit = 1000/this.rate;
     }
     //Particle customization variables
     private pitch : number = 0;
@@ -39,8 +40,15 @@ class Emitter {
         ) );
     }
 
+    private nextEmit : number;
+    private time : number = 0;
     public update(deltaTime : number) {
-
+        this.time += deltaTime;
+        if(this.time > this.nextEmit) {
+            this.emit();
+            this.time = this.time - this.nextEmit;
+            this.nextEmit = 1000/(this.rate + Rng.var(this.rateVar));
+        }
 
         this.particles.forEach(p => {
             p.update(deltaTime);
