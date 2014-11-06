e2bf936f29b62f12f6dd5eb6ce40e627d53deda9
Nikolaj K. Falsted <nikolajfalsted@gmail.com>
2014-11-06 23:54:54 +0100

    Optimizations, Vector now mutable, Removed array allocations when collecting data

--- Particle-System/Particle.ts
+++ Particle-System/Particle.ts
@@ -4,21 +4,25 @@
 class Particle {
     public prevPos : Vector;
     public prevColor : Color;
-    constructor(
-        public pos : Vector,
-        public dir : Vector,
-        public life : number,
-        public color : Color
-    ) {
+    public pos : Vector;
+    public dir : Vector;
+    public life : number = 0;
+    public color : Color;
+
+    public set(pos : Vector, dir : Vector, life : number, color : Color) {
+        this.pos = pos;
+        this.dir = dir;
+        this.life = life;
+        this.color = color;
         this.prevPos = pos;
         this.prevColor = color;
     }
 
     public update(deltaTime: number, force : Vector) : boolean {
-        this.prevPos =  this.pos;
-        this.pos =      this.pos.add(this.dir.mul(deltaTime));
-        this.dir =      this.dir.add(force.mul(deltaTime));
-        this.prevColor= this.color;
+        this.prevPos =  this.pos.copy();
+        this.pos.addmul(this.dir, deltaTime);
+        this.dir.addmul(force, deltaTime);
+        this.prevColor = this.color;
         this.color =    new  Color();
         this.life -= deltaTime;
         return this.life > 0;
@@ -26,5 +30,8 @@ class Particle {
 
     public collectDrawData() : Vector {
         return this.pos;
+    }
+    public isDead() : boolean {
+        return this.life <= 0;
     }
 }
